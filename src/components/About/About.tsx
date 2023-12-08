7c657676d46eb6de161f4ad6effd1132b21bc890
Tsvetan Eftenov <105812168+eftenow@users.noreply.github.com>
2023-12-08 09:21:40 +0200

    added everything for the about me page

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 const About = () => {
   return (
-    <section id='about' className='section container bg-dark-1'>
+    <section id='about' className='section bg-dark-1'>
+      <div className='container'>
       <header className='about-header-wrapper'>
         <h2 className='section-background-header'>ABOUT ME</h2>
         <h3 className='section-header'>Who am I?</h3>
       </header>
       <article className='row about-me-content'>
-        <article className='about-me-text'>
+        <article className='about-me-text primary-column'>
           <h2>I'm <span className='text-primary '>Tsvetan Eftenov,</span> a Web Developer</h2>
           <p>I help you build brand for your business at an affordable price. Thousands of clients have procured exceptional results while working with our dedicated team. when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
           <p>Delivering work within time and budget which meets client’s requirements is our moto. Lorem Ipsum has been the industry's standard dummy text ever when an unknown printer took a galley.</p>
@@ -24,6 +25,7 @@ const About = () => {
           <a href="#" className='btn btn-primary'>Download CV</a>
         </section>
       </article>
+      </div>
     </section>
   )
 }
