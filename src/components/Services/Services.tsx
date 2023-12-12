f017d52a3a7f99b9633267163d99365bb7ca353d
Tsvetan Eftenov <105812168+eftenow@users.noreply.github.com>
2023-12-12 10:11:23 +0200

    added service box and styles to it

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -1,24 +1,33 @@
 import React from 'react'
 
 const Services = () => {
-  return (
-    <section id='services' className='section bg-dark-2'>
-        <div className="container">
-        <header className='header-wrapper'>
-          <h2 className='section-background-header'>EXPERTISE</h2>
-          <h3 className='section-header'>What I Do?
-            <span className='heading-separator'></span>
-          </h3>
-        </header>
+    return (
+        <section id='services' className='section bg-dark-2'>
+            <div className="container">
+                <header className='header-wrapper'>
+                    <h2 className='section-background-header'>EXPERTISE</h2>
+                    <h3 className='section-header'>What I Do?
+                        <span className='heading-separator'></span>
+                    </h3>
+                </header>
 
-        <section className='wide-column'>
-            <div className='row'>
+                <section className='wide-column'>
+                    <ul className='row'>
+                        <li className='half-row-column service-box'>
+                            <div className='service-box-icon-wrapper bg-dark-1'>
+                                <i className="fa-solid fa-palette"></i>
+                            </div>
+                            <article>
+                                <h3 className='service-title'>Full-Stack Web Development</h3>
+                                <p className='service-text'>End-to-end solutions from concept to launch with cutting-edge tech.</p>
+                            </article>
+                        </li>
 
+                    </ul>
+                </section>
             </div>
         </section>
-        </div>
-    </section>
-  )
+    )
 }
 
 export default Services
