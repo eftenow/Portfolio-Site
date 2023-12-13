a5ed50d8910a2ba476856703633bfcef8f7398d7
Tsvetan Eftenov <105812168+eftenow@users.noreply.github.com>
2023-12-13 09:37:54 +0200

    fixed a bug with the service containers

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -11,19 +11,51 @@ const Services = () => {
                     </h3>
                 </header>
 
-                <section className='wide-column'>
-                    <ul className='row'>
-                        <li className='half-row-column service-box'>
-                            <div className='service-box-icon-wrapper bg-dark-1'>
-                                <i className="fa-solid fa-palette"></i>
-                            </div>
-                            <article>
-                                <h3 className='service-title'>Full-Stack Web Development</h3>
-                                <p className='service-text'>End-to-end solutions from concept to launch with cutting-edge tech.</p>
-                            </article>
-                        </li>
+                <section className='row'>
+                    <div className='wide-column'>
+                        <ul className="row">
+                            <li className='half-row-column service-box'>
+                                <div className='service-box-icon-wrapper bg-dark-1'>
+                                    <i className="fa-solid fa-palette"></i>
+                                </div>
+                                <article>
+                                    <h3 className='service-title'>Full-Stack Web Development</h3>
+                                    <p className='service-text'>End-to-end solutions from concept to launch with cutting-edge tech.</p>
+                                </article>
+                            </li>
 
-                    </ul>
+                            <li className='half-row-column service-box'>
+                                <div className='service-box-icon-wrapper bg-dark-1'>
+                                    <i className="fa-solid fa-palette"></i>
+                                </div>
+                                <article>
+                                    <h3 className='service-title'>Full-Stack Web Development</h3>
+                                    <p className='service-text'>End-to-end solutions from concept to launch with cutting-edge tech.</p>
+                                </article>
+                            </li>
+
+                            <li className='half-row-column service-box'>
+                                <div className='service-box-icon-wrapper bg-dark-1'>
+                                    <i className="fa-solid fa-palette"></i>
+                                </div>
+                                <article>
+                                    <h3 className='service-title'>Full-Stack Web Development</h3>
+                                    <p className='service-text'>End-to-end solutions from concept to launch with cutting-edge tech.</p>
+                                </article>
+                            </li>
+
+                            <li className='half-row-column service-box'>
+                                <div className='service-box-icon-wrapper bg-dark-1'>
+                                    <i className="fa-solid fa-palette"></i>
+                                </div>
+                                <article>
+                                    <h3 className='service-title'>Full-Stack Web Development</h3>
+                                    <p className='service-text'>End-to-end solutions from concept to launch with cutting-edge tech.</p>
+                                </article>
+                            </li>
+                        </ul>
+
+                    </div>
                 </section>
             </div>
         </section>
