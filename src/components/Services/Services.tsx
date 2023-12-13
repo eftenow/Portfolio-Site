fb302ab8a40cd1b83f6db4e2aced2e974f87fe4a
Tsvetan Eftenov <105812168+eftenow@users.noreply.github.com>
2023-12-13 10:01:59 +0200

    added suitable icons and text to the services

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -14,46 +14,78 @@ const Services = () => {
                 <section className='row'>
                     <div className='wide-column'>
                         <ul className="row">
-                            <li className='half-row-column service-box'>
+                            {/* <li className='half-row-column service-box'>
                                 <div className='service-box-icon-wrapper bg-dark-1'>
-                                    <i className="fa-solid fa-palette"></i>
+                                <i className="fa-solid fa-code"></i>
                                 </div>
                                 <article>
                                     <h3 className='service-title'>Full-Stack Web Development</h3>
-                                    <p className='service-text'>End-to-end solutions from concept to launch with cutting-edge tech.</p>
+                                    <p className='service-text'>Providing complete web solutions from initial concept to final launch, using modern technologies.</p>
+                                </article>
+                            </li> */}
+                            <li className='half-row-column service-box'>
+                                <div className='service-box-icon-wrapper bg-dark-1'>
+                                    <i className="fa-solid fa-code"></i>
+                                </div>
+                                <article>
+                                    <h3 className='service-title'>Front-End Development</h3>
+                                    <p className='service-text'>Specializing in creating responsive and engaging user interfaces with JavaScript, React, and Svelte.</p>
                                 </article>
                             </li>
+
+                            <li className='half-row-column service-box'>
+                                <div className='service-box-icon-wrapper bg-dark-1'>
+                                    <i className="fa-solid fa-server"></i>
+                                </div>
+                                <article>
+                                    <h3 className='service-title'>Back-End Development</h3>
+                                    <p className='service-text'>Expertise in NodeJS and Python to build efficient, scalable server-side applications for diverse needs.</p>
+                                </article>
+                            </li>
+
+                            <li className='half-row-column service-box'>
+                                <div className='service-box-icon-wrapper bg-dark-1'>
+                                    <i className="fa-solid fa-gears"></i>
+                                </div>
+                                <article>
+                                    <h3 className='service-title'>API Development & Integration</h3>
+                                    <p className='service-text'>Developing and integrating custom APIs to enhance and streamline web application functionalities.</p>
+                                </article>
+                            </li>
+
+                            <li className='half-row-column service-box'>
+                                <div className='service-box-icon-wrapper bg-dark-1'>
+                                    <i className="fa-solid fa-database"></i>
+                                </div>
+                                <article>
+                                    <h3 className='service-title'>Database Management</h3>
+                                    <p className='service-text'>Designing and optimizing databases to elevate your data systems for peak efficiency and performance.</p>
+                                </article>
+                            </li>
+
 
                             <li className='half-row-column service-box'>
                                 <div className='service-box-icon-wrapper bg-dark-1'>
                                     <i className="fa-solid fa-palette"></i>
                                 </div>
                                 <article>
-                                    <h3 className='service-title'>Full-Stack Web Development</h3>
-                                    <p className='service-text'>End-to-end solutions from concept to launch with cutting-edge tech.</p>
+                                    <h3 className='service-title'>Responsive Web Design</h3>
+                                    <p className='service-text'>Designing visually appealing and functional websites that provide a seamless experience across all devices.</p>
                                 </article>
                             </li>
 
-                            <li className='half-row-column service-box'>
-                                <div className='service-box-icon-wrapper bg-dark-1'>
-                                    <i className="fa-solid fa-palette"></i>
-                                </div>
-                                <article>
-                                    <h3 className='service-title'>Full-Stack Web Development</h3>
-                                    <p className='service-text'>End-to-end solutions from concept to launch with cutting-edge tech.</p>
-                                </article>
-                            </li>
 
                             <li className='half-row-column service-box'>
                                 <div className='service-box-icon-wrapper bg-dark-1'>
-                                    <i className="fa-solid fa-palette"></i>
+                                    <i className="fa-solid fa-code-branch"></i>
                                 </div>
                                 <article>
-                                    <h3 className='service-title'>Full-Stack Web Development</h3>
-                                    <p className='service-text'>End-to-end solutions from concept to launch with cutting-edge tech.</p>
+                                    <h3 className='service-title'>Version Control & Project Management</h3>
+                                    <p className='service-text'>Utilizing Git and Jira for effective version control and project management, ensuring smooth team collaboration.</p>
                                 </article>
                             </li>
                         </ul>
+
 
                     </div>
                 </section>
