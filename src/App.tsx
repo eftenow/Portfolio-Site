cd0756f0e7839c6c6a6d1eb88d0089ba6521603a
Tsvetan Eftenov <105812168+eftenow@users.noreply.github.com>
2023-12-14 11:28:18 +0200

    qualifications update

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import About from "./components/About/About"
 import { Home } from "./components/Home/Home"
 import { Navigation } from "./components/Navigation/Navigation"
 import Services from "./components/Services/Services"
+import Qualifications from "./components/Summary/QUALIFICATIONS"
+
 
 function App() {
 
@@ -12,6 +14,7 @@ function App() {
           <Home />
           <About />
           <Services />
+          <Qualifications />
         </main>
       </div>
   )
