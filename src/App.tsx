2b7e9a5d619de749745f3b8f6e7d8b052d27d89a
Tsvetan Eftenov <105812168+eftenow@users.noreply.github.com>
2024-01-03 08:12:31 +0200

    added contact section html structure

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Services from "./components/Services/Services"
 import Qualifications from "./components/Qualifications/Qualifications"
 import Tools from "./components/Tools/Tools"
 import Portfolio from "./components/Portfolio/Portfolio"
+import Contact from "./components/Contact/Contact"
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
           <Qualifications />
           <Tools />
           <Portfolio />
+          <Contact />
         </main>
       </div>
   )
---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <section id='contact' className='section bg-dark-2'>
+            <div className='container'>
+                <header className='header-wrapper'>
+                    <h2 className='section-background-header'>CONTACT</h2>
+                    <h3 className='section-header'>Get In Touch
+                        <span className='heading-separator'></span>
+                    </h3>
+                </header>
+                    <section className='contact-me-content row'>
+                        <ul className='column address-info'>
+                            <li className='call'>
+                                <i className="fa-solid fa-phone"></i>
+                                <div>
+                                    <h3>Call Me</h3>
+                                    <p>088 4292 387</p>
+                                </div>
+                            </li>
+
+                            <li className='email'>
+                                <i className="fa-solid fa-envelope"></i>
+                                <div>
+                                    <h3>Email</h3>
+                                    <p>tsvetan.eftenov@gmail.com</p>
+                                </div>
+                            </li>
+
+                            <li className='email'>
+                            <i className="fa-solid fa-location-dot"></i>
+                                <div>
+                                    <h3>Location</h3>
+                                    <p>Veliko Tarnovo, Bulgaria</p>
+                                </div>
+                            </li>
+                        </ul>
+                        <div className='form-wrapper'>
+                            <h3>Message me</h3>
+                            <form className='form-dark' id='contact-form'>
+                                <input name='username' type="text" className='form-field' placeholder='Name' />
+                                <input name='email' type="email" className='form-field' placeholder='Email' />
+                                <textarea name='message' rows="10" className='form-field' placeholder='Message'></textarea>
+                            </form>
+                        </div>
+                    </section>
+            </div>
+        </section>
+    )
+}
+
+export default Contact
