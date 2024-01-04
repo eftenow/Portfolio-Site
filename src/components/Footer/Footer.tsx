b1c5a003eb0794cbd9f176d935d1c5aef3994e62
Tsvetan Eftenov <105812168+eftenow@users.noreply.github.com>
2024-01-04 18:15:58 +0200

    added all footer links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,13 +3,42 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer id='footer' className='footer-dark bg-dark-1'>
-            <div>
-                <h3>Follow me</h3>
-                <ul className="side-nav-bot">
-                    <li className='media-btn'><a href="https://github.com/" className="media-icon"><i className="uil uil-message"></i></a></li>
-                    <li className='media-btn'><a href="https://www.linkedin.com/" className="media-icon"><i className="uil uil-linkedin"></i></a></li>
-                </ul>
-            </div>
+            <h3>Follow me</h3>
+            <ul className="social-links">
+
+                <li className='media-link github'>
+                    <div className='animate_fade'>
+                        <a href="https://github.com/eftenow" className="media-icon"><i className="fa-brands fa-github"></i></a>
+                    </div>
+                </li>
+
+                <li className='media-link facebook'>
+                    <div className='animate_fade'>
+                        <a href="#" className="media-icon"><i className="fa-brands fa-facebook"></i></a>
+                    </div>
+                </li>
+
+                <li className='media-link gitlab'>
+                    <div className='animate_fade'>
+                        <a href="https://gitlab.com/eftenow" className="media-icon"><i className="fa-brands fa-gitlab"></i></a>
+                    </div>
+                </li>
+
+                <li className='media-link linkedin'>
+                    <div className='animate_fade'>
+                        <a href="https://www.linkedin.com/" className="media-icon"><i className="uil uil-linkedin"></i></a>
+                    </div>
+                </li>
+
+                <li className='media-link github'>
+                    <div className='animate_fade'>
+                        <a href="https://github.com/" className="media-icon"><i className="uil uil-message"></i></a>
+                    </div>
+                </li>
+
+
+
+            </ul>
         </footer>
     )
 }
