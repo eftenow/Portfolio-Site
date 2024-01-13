0ceda7fada69b457affbb34f8fe02d770e7b5400
Tsvetan Eftenov <mass4cre@icloud.com>
2024-01-13 14:48:12 +0200

    added formspree to the project, which takes care of email sending

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import { useForm } from '@formspree/react';
 
 const Contact = () => {
+    const [state, handleSubmit] = useForm("xdoqkgyz");
+
+    if (state.succeeded) {
+        return <p>Thanks for joining!</p>;
+    }
+
     return (
         <section id='contact' className='section bg-dark-2'>
             <div className='container'>
@@ -10,42 +16,43 @@ const Contact = () => {
                         <span className='heading-separator'></span>
                     </h3>
                 </header>
-                    <section className='contact-me-content row'>
-                        <ul className='address-info'>
-                            <li className='call'>
-                                <i className="fa-solid fa-phone"></i>
-                                <div>
-                                    <h3>Call Me</h3>
-                                    <p>088 4292 387</p>
-                                </div>
-                            </li>
+                <section className='contact-me-content row'>
+                    <ul className='address-info'>
+                        <li className='call'>
+                            <i className="fa-solid fa-phone"></i>
+                            <div>
+                                <h3>Call Me</h3>
+                                <p>088 4292 387</p>
+                            </div>
+                        </li>
 
-                            <li className='email'>
-                                <i className="fa-solid fa-envelope"></i>
-                                <div>
-                                    <h3>Email</h3>
-                                    <p>tsvetan.eftenov@gmail.com</p>
-                                </div>
-                            </li>
+                        <li className='email'>
+                            <i className="fa-solid fa-envelope"></i>
+                            <div>
+                                <h3>Email</h3>
+                                <p>tsvetan.eftenov@gmail.com</p>
+                            </div>
+                        </li>
 
-                            <li className='location'>
+                        <li className='location'>
                             <i className="fa-solid fa-location-dot"></i>
-                                <div>
-                                    <h3>Location</h3>
-                                    <p>Veliko Tarnovo, Bulgaria</p>
-                                </div>
-                            </li>
-                        </ul>
-                        <div className='form-wrapper'>
-                            <h3>Message me</h3>
-                            <form className='form-dark' id='contact-form'>
-                                <input name='username' type="text" className='form-field' placeholder='Name' />
-                                <input name='email' type="email" className='form-field' placeholder='Email' />
-                                <textarea name='message' className='form-field' placeholder='Message'></textarea>
-                                <button className='btn btn-primary'>Send</button>
-                            </form>
-                        </div>
-                    </section>
+                            <div>
+                                <h3>Location</h3>
+                                <p>Veliko Tarnovo, Bulgaria</p>
+                            </div>
+                        </li>
+                    </ul>
+                    <div className='form-wrapper'>
+                        <h3>Message me</h3>
+                        <form onSubmit={handleSubmit} className='form-dark' id='contact-form'>
+                            <input name='username' type="text" className='form-field' placeholder='Name' />
+                            <input name='email' type="email" className='form-field' placeholder='Email' />
+                            <textarea name='message' className='form-field' placeholder='Message'></textarea>
+
+                            <button type='submit' className='btn btn-primary' disabled={state.submitting}>Send</button>
+                        </form>
+                    </div>
+                </section>
             </div>
         </section>
     )
