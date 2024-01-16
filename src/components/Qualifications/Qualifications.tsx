ca06ead39de8aee4ee72fe6c42786193ab3a02b6
eftenow <mass4cre@icloud.com>
2024-01-16 18:27:57 +0200

    qualifications now work with json data

--- src/components/Qualifications/Qualifications.tsx
+++ src/components/Qualifications/Qualifications.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+import qualificationsData from "./../../data/qualificationsData.json";
 import titles from "./../../data/titles.json";
 import buttons from "./../../data/buttons.json";
 
@@ -18,18 +19,15 @@ const Qualifications = () => {
         <section id='experience' className='section bg-dark-1'>
             <div className='container'>
                 <header className='header-wrapper'>
-                    <h2 className='section-background-header'>{titles.about.title}</h2>
-                    <h3 className='section-header'>{titles.about.title}
+                    <h2 className='section-background-header'>{titles.qualifications.title}</h2>
+                    <h3 className='section-header'>{titles.qualifications.subtitle}
                         <span className='heading-separator'></span>
                     </h3>
                 </header>
 
                 <p className="introduction-paragraph qual-paragraph">
-                    <span>Discover my journey</span> This section unveils the landmarks of my education and career. Explore the key experiences that have sculpted my journey in the tech world.
+                    <span>{qualificationsData.introTitle}</span> {qualificationsData.introParagraph}
                 </p>
-
-
-
 
                 <section className="qualification-btns">
                     <button className={`qualification-btn ${activeSection === 'education' ? 'qualification-active' : ''}`}
@@ -45,79 +43,61 @@ const Qualifications = () => {
                     </button>
                 </section>
 
-                {/* EDUCATION */}
                 <ul className={`qualification-content ${activeSection === 'education' ? 'qualification-active' : ''}`} data-content id="education">
-                    {/* QUALIFICATION 1 */}
-                    <li className="qualification-data">
-                        <article className="qualification-box">
-                            <h4>Banking & Economics</h4>
-                            <p>Professional High School of Economics</p>
-                            <div className="qualification-calendar">
-                                <i className="fa-regular fa-calendar"></i>
-                                2013 - 2017
-                            </div>
-                        </article>
+                    {qualificationsData.education.map((card, index) => (
+                        <li key={(index)} className="qualification-data">
+                            {index % 2 != 0 && <>
+                                <div></div>
+                                <div>
+                                    <span className="qualification-rounder"></span>
+                                    <span className="qualification-line"></span>
+                                </div></>}
+                            <article className="qualification-box">
+                                <h4>{card.title}</h4>
+                                <p>{card.subtitle}</p>
+                                <div className="qualification-calendar">
+                                    <i className="fa-regular fa-calendar"></i>
+                                    {card.years}
+                                </div>
+                            </article>
 
-                        <div>
-                            <span className="qualification-rounder"></span>
-                            <span className="qualification-line"></span>
-                        </div>
-                    </li>
-
-                    {/* QUALIFICATION 2 */}
-                    <li className="qualification-data">
-                        <div></div>
-                        <div>
-                            <span className="qualification-rounder"></span>
-                            <span className="qualification-line"></span>
-                        </div>
-
-                        <article>
-                            <h4>Linguistics with English and IT</h4>
-                            <p>Veliko Tarnovo University</p>
-                            <div className="qualification-calendar">
-                                <i className="fa-regular fa-calendar"></i>
-                                2017 - 2022
-                            </div>
-                        </article>
-                    </li>
-
-                    {/* QUALIFICATION 3 */}
-                    <li className="qualification-data">
-
-                        <article className="qualification-box">
-                            <h4>Web Developement</h4>
-                            <p>Software University</p>
-                            <div className="qualification-calendar">
-                                <i className="fa-regular fa-calendar"></i>
-                                2022 - 2023
-                            </div>
-                        </article>
-                        <div>
-                            <span className="qualification-rounder"></span>
-                        </div>
-                    </li>
+                            {index % 2 == 0 &&
+                                <div>
+                                    <span className="qualification-rounder"></span>
+                                    
+                                    {index < qualificationsData.education.length - 1 &&
+                                    <span className="qualification-line"></span>}
+                                </div>}
+                        </li>
+                    ))}
                 </ul>
 
-                {/* WORK */}
-                <section className={`qualification-content ${activeSection === 'work' ? 'qualification-active' : ''}`} data-content id="work">
-                    {/* WORK-QUALIFICATION 1 */}
-                    <li className="qualification-data">
-                        <article className="qualification-box">
-                            <h4>Web Developer</h4>
-                            <p>ZestLabs</p>
-                            <div className="qualification-calendar">
-                                <i className="fa-regular fa-calendar"></i>
-                                2023 - Present
-                            </div>
-                        </article>
+                <ul className={`qualification-content ${activeSection === 'work' ? 'qualification-active' : ''}`} data-content id="work">
+                {qualificationsData.work.map((card, index) => (
+                        <li key={(index)} className="qualification-data">
+                            {index % 2 != 0 && <>
+                                <div></div>
+                                <div>
+                                    <span className="qualification-rounder"></span>
+                                    <span className="qualification-line"></span>
+                                </div></>}
+                            <article className="qualification-box">
+                                <h4>{card.title}</h4>
+                                <p>{card.subtitle}</p>
+                                <div className="qualification-calendar">
+                                    <i className="fa-regular fa-calendar"></i>
+                                    {card.years}
+                                </div>
+                            </article>
 
-                        <div>
-                            <span className="qualification-rounder"></span>
-                            <span className="qualification-line"></span>
-                        </div>
-                    </li>
-                </section>
+                            {index % 2 == 0 &&
+                                <div>
+                                    <span className="qualification-rounder"></span>
+                                    <span className="qualification-line"></span>
+                                </div>}
+                        </li>
+                    ))}
+                </ul>
 
                 <div className="cv-btn-wrapper">
                     <a href={buttons.cv} className='btn btn-primary' rel="noreferrer" target="_blank">
