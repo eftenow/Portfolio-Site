af8755a02efff8d08abdb7a865bd96519522d66d
eftenow <mass4cre@icloud.com>
2024-01-16 18:41:42 +0200

    tools section now uses json data

--- src/components/Tools/Tools.tsx
+++ src/components/Tools/Tools.tsx
@@ -1,4 +1,5 @@
 import titles from "./../../data/titles.json";
+import toolsData from "./../../data/toolsData.json";
 
 const Tools = () => {
     return (
@@ -13,69 +14,14 @@ const Tools = () => {
 
 
                 <section className="row skills-section">
-                    <p className="introduction-paragraph"><span>Welcome to my digital toolbox</span> Here, you'll find a collection of magical tools and technologies that I wield to create, innovate in the digital realm. Each skill allows me to bring ideas to life.</p>
+                    <p className="introduction-paragraph"><span>{toolsData.introTitle}</span> {toolsData.introParagraph}</p>
                     <ul className="skill-box-wrapper row">
-                        <li className="skill-box bg-dark">
-                            <i className="devicon-javascript-plain"></i>
-                            <h4>JavaScript</h4>
-                            <p>Conjuring interactive web experiences with JavaScript's spellbinding syntax!</p>
-                        </li>
-                        <li className="skill-box bg-dark">
-                            <i className="devicon-react-original"></i>
-                            <h4>React</h4>
-                            <p>Transforming UI designs into vibrant, living canvases with React's magic.</p>
-                        </li>
-
-                        <li className="skill-box bg-dark">
-                            <i className="devicon-python-plain"></i>
-                            <h4>Python</h4>
-                            <p>Unlocking mysteries of data and automation with Python's elegant code potions.</p>
-                        </li>
-                        <li className="skill-box bg-dark">
-                            <i className="devicon-django-plain"></i>
-                            <h4>Django</h4>
-                            <p>Orchestrating web symphonies with Django's powerful and scalable notes.</p>
-                        </li>
-                        <li className="skill-box bg-dark">
-                            <i className="devicon-nodejs-plain"></i>
-                            <h4>NodeJS</h4>
-                            <p>Pioneering server-side realms with NodeJS's event-driven alchemy.</p>
-                        </li>
-                        <li className="skill-box bg-dark">
-                            <i className="devicon-express-original"></i>
-                            <h4>Express</h4>
-                            <p>Accelerating web application highways with Express's sleek and efficient middleware.</p>
-                        </li>
-                        <li className="skill-box bg-dark">
-                            <i className="devicon-mysql-plain"></i>
-                            <h4>MySQL</h4>
-                            <p>Navigating the seas of data with MySQL's functionalities and tools.</p>
-                        </li>
-                        <li className="skill-box bg-dark">
-                            <i className="devicon-postgresql-plain"></i>
-                            <h4>PostgreSQL</h4>
-                            <p>Mastering the art of data wizardry with PostgreSQL's advanced enchantments.</p>
-                        </li>
-                        <li className="skill-box bg-dark">
-                            <i className="devicon-mocha-plain"></i>
-                            <h4>Mocha</h4>
-                            <p>Ensuring our code's strength and resilience with Mocha's testing elixirs.</p>
-                        </li>
-                        <li className="skill-box bg-dark">
-                            <i className="devicon-jest-plain"></i>
-                            <h4>JEST</h4>
-                            <p>Sharpening JavaScript's performance edge with JEST's precision testing tools.</p>
-                        </li>
-                        <li className="skill-box bg-dark">
-                            <i className="devicon-git-plain"></i>
-                            <h4>Git</h4>
-                            <p>Mastering the time-travel art of version control with Git's powerful chronomancy.</p>
-                        </li>
-                        <li className="skill-box bg-dark">
-                            <i className="devicon-jira-plain"></i>
-                            <h4>Jira</h4>
-                            <p>Choreographing the dance of project management with Jira's strategic symphony.</p>
-                        </li>
+                        {toolsData.cards.map(card => (
+                            <li className="skill-box bg-dark">
+                                <i className={card.icon}></i>
+                                <h4>{card.title}</h4>
+                                <p>{card.description}</p>
+                            </li>))}
                     </ul>
 
                 </section>
