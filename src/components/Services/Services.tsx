6c5b2448fbe42e730f2fb0251cc1a76a85697c44
eftenow <mass4cre@icloud.com>
2024-01-16 10:01:52 +0200

    services section now gets its data from json file

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -1,4 +1,5 @@
 import titles from "./../../data/titles.json";
+import servicesData from "./../../data/servicesData.json";
 
 const Services = () => {
     return (
@@ -16,79 +17,20 @@ const Services = () => {
                         <span><span>Explore my craft</span></span>Embark on a journey through my digital craftsmanship, where technology meets creativity. Below are the realms of my expertise:
                     </p>
                     <div className='wide-column'>
+
                         <ul className="row">
-                            {/* <li className='half-row-column service-box'>
-                                <div className='service-box-icon-wrapper bg-dark-1'>
-                                <i className="fa-solid fa-code"></i>
-                                </div>
-                                <article>
-                                    <h3 className='service-title'>Full-Stack Web Development</h3>
-                                    <p className='service-text'>Providing complete web solutions from initial concept to final launch, using modern technologies.</p>
-                                </article>
-                            </li> */}
-                            <li className='half-row-column service-box'>
-                                <div className='service-box-icon-wrapper bg-dark-1'>
-                                    <i className="fa-solid fa-code"></i>
-                                </div>
-                                <article>
-                                    <h3 className='service-title'>Front-End Development</h3>
-                                    <p className='service-text'>Specializing in creating responsive and engaging user interfaces with JavaScript, React, and Svelte.</p>
-                                </article>
-                            </li>
-
-                            <li className='half-row-column service-box'>
-                                <div className='service-box-icon-wrapper bg-dark-1'>
-                                    <i className="fa-solid fa-server"></i>
-                                </div>
-                                <article>
-                                    <h3 className='service-title'>Back-End Development</h3>
-                                    <p className='service-text'>Expertise in NodeJS and Python to build efficient, scalable server-side applications for diverse needs.</p>
-                                </article>
-                            </li>
-
-                            <li className='half-row-column service-box'>
-                                <div className='service-box-icon-wrapper bg-dark-1'>
-                                    <i className="fa-solid fa-gears"></i>
-                                </div>
-                                <article>
-                                    <h3 className='service-title'>API Development & Integration</h3>
-                                    <p className='service-text'>Developing and integrating custom APIs to enhance and streamline web application functionalities.</p>
-                                </article>
-                            </li>
-
-                            <li className='half-row-column service-box'>
-                                <div className='service-box-icon-wrapper bg-dark-1'>
-                                    <i className="fa-solid fa-database"></i>
-                                </div>
-                                <article>
-                                    <h3 className='service-title'>Database Management</h3>
-                                    <p className='service-text'>Designing and optimizing databases to elevate your data systems for peak efficiency and performance.</p>
-                                </article>
-                            </li>
-
-
-                            <li className='half-row-column service-box'>
-                                <div className='service-box-icon-wrapper bg-dark-1'>
-                                    <i className="fa-solid fa-palette"></i>
-                                </div>
-                                <article>
-                                    <h3 className='service-title'>Responsive Web Design</h3>
-                                    <p className='service-text'>Designing visually appealing and functional websites that provide a seamless experience across all devices.</p>
-                                </article>
-                            </li>
-
-
-                            <li className='half-row-column service-box'>
-                                <div className='service-box-icon-wrapper bg-dark-1'>
-                                    <i className="fa-solid fa-code-branch"></i>
-                                </div>
-                                <article>
-                                    <h3 className='service-title'>Version Control & Project Management</h3>
-                                    <p className='service-text'>Utilizing Git and Jira for effective project management, ensuring smooth team collaboration.</p>
-                                </article>
-                            </li>
+                            {servicesData.cards.map((card, index) => (
+                                <li key={index} className='half-row-column service-box'>
+                                    <div className='service-box-icon-wrapper bg-dark-1'>
+                                        <i className={card.icon}></i>
+                                    </div>
+                                    <article>
+                                        <h3 className='service-title'>{card.title}</h3>
+                                        <p className='service-text'>{card.description}</p>
+                                    </article>
+                                </li>
+                            ))}
                         </ul>
-
 
                     </div>
                 </section>
